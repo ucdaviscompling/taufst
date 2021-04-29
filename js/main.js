797653c549f6390e0de9af336876e65c1baced53
Kenji Sagae <sagae@Kenjis-MacBook-Pro.local>
2021-04-29 00:49:19 -0700

    added a limit of 10 for consecutive eps:eps transitions

--- js/main.js
+++ js/main.js
@@ -88,47 +88,49 @@ function newFile() {
 
 function consultFST() {
 	var fststr = `
-:- use_module(library(lists)).
+	:- use_module(library(lists)).
 
-% This is what we use to run the transducer
-fst(Input, Output) :-
-  initial(State),
-  go(State, Input, Output).
-
-% This is how we know that we've reached the end of the run
-go(CurrentState, [], []) :-
-  final(CurrentState).
-
-% Use a transition with no eps on either side
-go(CurrentState, [A|InString], [B|OutString]) :-
-  transition(CurrentState, A, NextState, B),
-  A \\= eps,
-  B \\= eps,
-  go(NextState, InString, OutString).
-
-% use a transition with eps on the input side
-go(CurrentState, InString, [B|OutString]) :-
-  transition(CurrentState, eps, NextState, B),
-  B \\= eps,
-  go(NextState, InString, OutString).
-
-% use a transition with eps on the output side
-go(CurrentState, [A|InString], OutString) :-
-  transition(CurrentState, A, NextState, eps),
-  A \\= eps,
-  go(NextState, InString, OutString).
-
-% use a transition with eps on both sides
-go(CurrentState, InString, OutString) :-
-  transition(CurrentState, eps, NextState, eps),
-  go(NextState, InString, OutString).
-
-% FSA is an FST with the same input and output
-fsa(X) :- fst(X, X).
-
-% An FSA transition is a transition with the
-% same input and output symbol
-transition(State, Sym, NextState, Sym) :- transition(State, Sym, NextState).
+	% This is what we use to run the transducer
+	fst(Input, Output) :-
+	  initial(State),
+	  go(State, Input, Output, 0).
+	
+	% This is how we know that we've reached the end of the run
+	go(CurrentState, [], [], _) :-
+	  final(CurrentState).
+	
+	% Use a transition with no eps on either side
+	go(CurrentState, [A|InString], [B|OutString], _) :-
+	  transition(CurrentState, A, NextState, B),
+	  A \\= eps,
+	  B \\= eps,
+	  go(NextState, InString, OutString, 0).
+	
+	% use a transition with eps on the input side
+	go(CurrentState, InString, [B|OutString], _) :-
+	  transition(CurrentState, eps, NextState, B),
+	  B \\= eps,
+	  go(NextState, InString, OutString, 0).
+	
+	% use a transition with eps on the output side
+	go(CurrentState, [A|InString], OutString, _) :-
+	  transition(CurrentState, A, NextState, eps),
+	  A \\= eps,
+	  go(NextState, InString, OutString, 0).
+	
+	% use a transition with eps on both sides
+	go(CurrentState, InString, OutString, N) :-
+	  transition(CurrentState, eps, NextState, eps),
+	  N < 10,
+	  M is N + 1,
+	  go(NextState, InString, OutString, M).
+	
+	% FSA is an FST with the same input and output
+	fsa(X) :- fst(X, X).
+	
+	% An FSA transition is a transition with the
+	% same input and output symbol
+	transition(State, Sym, NextState, Sym) :- transition(State, Sym, NextState).
   `;
 
 	var parsed = session.consult(fststr);
